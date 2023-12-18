54422d37b8942d986d27637455098d1684dd8735
alinrmkh <amanbekovaalina10@gmail.com>
2023-12-18 18:20:54 +0600

    add_notification

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -12,35 +12,35 @@ const loginPage = new LoginPage();
 
 
 
-  it(' Not Authorized to page clients', () => {
+  // it(' Not Authorized to page clients', () => {
  
-    loginPage.doLogin('Invalid','Admin@Navi');
-    cy.url().should('include', 'login');  
-  });
+  //   loginPage.doLogin('Invalid','Admin@Navi');
+  //   cy.url().should('include', 'login');  
+  // });
 
 
 
-  it(' Not Authorized to page clients', () => {
+  // it(' Not Authorized to page clients', () => {
 
-    loginPage.doLogin('Admin','Invalid');
+  //   loginPage.doLogin('Admin','Invalid');
 
-    cy.url().should('include', 'login');  
-    // Visit the main page
-    cy.visit('http://167.114.201.175:5000/');
+  //   cy.url().should('include', 'login');  
+  //   // Visit the main page
+  //   cy.visit('http://167.114.201.175:5000/');
 
-    // Enter login credentials and submit the form
-    cy.wait(1000);
-    cy.get('input[name="userName"]').type('Admin');
-    cy.wait(1000);
-    cy.get('input[name="password"]').type('Invalid');
-    cy.wait(1000);
-    cy.get('button[type="submit"]').click();
-    cy.wait(1000);
+  //   // Enter login credentials and submit the form
+  //   cy.wait(1000);
+  //   cy.get('input[name="userName"]').type('Admin');
+  //   cy.wait(1000);
+  //   cy.get('input[name="password"]').type('Invalid');
+  //   cy.wait(1000);
+  //   cy.get('button[type="submit"]').click();
+  //   cy.wait(1000);
 
-    cy.url().should('include', 'login');//expects asserts
-    cy.wait(1000);
+  //   cy.url().should('include', 'login');//expects asserts
+  //   cy.wait(1000);
   
-  });
+  // });
 
 
 
--- cypress/pageObjects/Notification.js
+++ cypress/pageObjects/Notification.js
@@ -1,41 +1,78 @@
-// cypress/support/pageObjects/MainPage.js
+export class Notification {
+  // Click on the 'Уведомления' link in the navigation
+  clickNotification() { return cy.get('.crm-navigation__item').contains('Уведомления').click({ force: true });}
+  // Click on the 'Добавить новое уведомление' button
+  addNotificationButton() {return cy.contains('Добавить новое уведомление').click({ force: true });}
+  // Check if the 'Новое сообщение' heading is visible
+  isNewMessageVisible() {return cy.get('.push-modal-form__heading').contains('Новое сообщение').should('be.visible');}
+  // Get the text field for the message
+  getMessageTextField() {return cy.get('.push-modal-form__heading').contains('Текс сообщения');}
+  // Open the dropdown for selecting notification type
+  openNotificationTypeDropdown() {return cy.get('.mat-select').click();}
+  // Select the 'SMS' option from the dropdown
+  selectSMSOption() { return cy.get('.mat-option').contains('SMS').click();}
 
-export class Notification{
+  doNotification() {
+    this.clickNotification();
+    cy.url().should('eq', 'http://167.114.201.175:5000/notifications');
+    cy.contains('Добавить новое уведомление').should('be.visible');
+    this.addNotificationButton();
+  
+    // Check if the 'Новое сообщение' heading is visible
+    this.isNewMessageVisible();
+  
+    // Type into the input field for the message subject
+    cy.get('.push-modal-add-title input').type('Alina');
+    cy.wait(1000);
+  
+    // Check if the 'Текс сообщения' heading is visible
+    cy.get('.push-modal-form__heading').contains('Текс сообщения').should('be.visible');
+    cy.wait(1000);
+  
+    // Type into the input field for the message body
+    cy.get('.push-modal-preview__text').eq(1).type('Alina');
+    cy.wait(1000);
+     // Select the 'SMS' option from the dropdown 
+    this.openNotificationTypeDropdown();
+    this.selectSMSOption();
+    cy.wait(1000);
 
-    clickNotification() { return cy.get('.crm-navigation__item').contains('Уведомления').click();}
-    addNotificationButton() { return cy.contains('Добавить новое уведомление').click({ force: true });}
-    newMessage(){return cy.get('.push-modal-form__heading').contains('Новое сообщение');}
-    //titleField(){return  cy.get('.push-modal-add-title input')} 
-    textField(){return cy.get('.push-modal-form__heading').contains('Текс сообщения')}
+    // Interact with the datepicker
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + 1);
+    const tomorrow = currentDate.toISOString().split('T')[0]; // Format as YYYY-MM-DD
+    cy.get('.push-modal-date input').type(tomorrow);
+  
+    // Open the "Группа пользователей" dropdown
+    cy.get('.push-modal-select-group__text').click();
     
-    newDropdown() {return   cy.get('.mat-select').click();}
-    newSMSclick(){return     cy.get('.mat-option').contains('SMS').click();}
+    // Choose "Template name" from the dropdown
+    cy.get('.push-modal-hidden-select__item').contains('Template name').click();
+  
+    // Check that "1 группы" is selected
+    cy.get('.push-modal-hidden__selected').should('contain', '1 группы');
+  
+    // Click the icon associated with "Template name"
+    cy.get('.material-icons').contains('done').click();
+  
+    // Continue with the rest of your notification creation steps
+   
+  
 
 
-
-    doNotification(){
-        this.clickNotification().contains('Уведомления').click();  
-        cy.url().should('eq', 'http://167.114.201.175:5000/notifications');
-        cy.wait(1000);
-        cy.contains('Добавить новое уведомление').should('be.visible');
-        cy.wait(1000);
-        this.addNotificationButton().click({ force: true });;
-        cy.wait(1000);
-      //  this.newMessage().contains('Новое сообщение');
-     //   cy.wait(1000); 
-
-
-      //  this.textField().contains('Текс сообщения');
-
-        
-
-
-    
-    
-    }
-     
-    
-    }
+    // Click the save button
+    cy.get('.push-col-save').should('be.visible').click();
     
     
     
+    
+    // Check for any additional errors or success messages
+    cy.on('window:alert', (str) => {
+      const errorData = JSON.parse(str);
+      // Add more assertions as needed
+    });
+
+  }
+  
+}
+ 
